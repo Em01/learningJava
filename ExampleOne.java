fdba576062b5505a70a217d3e758b96818b285bf
em01 <github@em01.co.uk>
2017-06-13 19:06:12 +0700

    Initial commit

---
+++ ExampleOne.java
@@ -0,0 +1,12 @@
+/*
+
+String firstName = console.readLine("What is your name?  ");
+console.printf("Hello my name is %s\n", firstName);
+
+Topics:
+* printf
+* console
+* i/o
+
+*/
+
---
+++ One.java
@@ -0,0 +1,22 @@
+/*
+
+Console is an object with methods to perform actions: prints to the screen
+console.printf("Hello");
+
+Compiling:
+javac One.java
+-this compiles and creates a .class file
+java one
+-this runs the executable file
+
+Declare Variables:
+
+String myName = "name"
+
+console.printf("my %s", myName)
+
+printf prints text to the screen
+readLine prints text to the screen and then captures whatever the user then types afterwards.
+
+i/o: inout/output
+*/
