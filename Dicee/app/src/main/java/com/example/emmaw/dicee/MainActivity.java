dfea12de7ab1a5730abe0ac30913884646001011
em01 <github@em01.co.uk>
2017-08-23 19:09:27 +0100

    Variables

--- Dicee/app/src/main/java/com/example/emmaw/dicee/MainActivity.java
+++ Dicee/app/src/main/java/com/example/emmaw/dicee/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.emmaw.dicee;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = (Button) findViewById(R.id.rollButton);
+
+        ImageView leftDice = (ImageView) findViewById(R.id.image_left_dice);
+        ImageView rightDice = (ImageView) findViewById(R.id.image_right_dice);
     }
 }
